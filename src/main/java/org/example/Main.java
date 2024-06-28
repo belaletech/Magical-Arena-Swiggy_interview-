e0772ce81815b5e7ecc0b77168c30b9136d0b96e
belaletech <belalahmadswnbhr@gmail.com>
2024-06-28 15:05:44 +0530

    Added main method to start the game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player playerA = new Player(50, 5, 10);
+        Player playerB = new Player(100, 10, 5);
+
+        Arena arena = new Arena();
+        arena.startFight(playerA, playerB);
     }
 }
